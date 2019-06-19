b1912a11ce22a34ca44cb255c23cc3fd59660c2c
makhambet <makha.98.29@mail.ru>
2019-06-19 11:27:33 +0600

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>2048</h1>
+    <p>Score: {{score}} Best: {{best}}</p>
+    <button @click="chip = true">New Game</button>
+    <br>
+    <div class="table">
+      <div class="box" v-for="a in newGameStart" :key="a">
+        <!-- <div v-if="chip || a===rand || a==2" class="chip">{{a}}</div> -->
+        <ceil v-if="chip || a===rand" :ceil="a"></ceil>  
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import ceil from './ceil'
+  export default {
+    data() {
+      return {
+        score: 0,
+        best: 0,
+        col: 16,
+        chip: false,
+        number: 2,
+        rand: 0,
+      }
+    },
+    computed: {
+      newGameStart() {
+        this.rand = Math.floor((Math.random() * 16) + 1);
+        console.log(this.rand)
+        return this.col
+      }
+    },
+    components: {
+      ceil,
+    },
+  }
+</script>
+
+<style scoped>
+  .table{
+    display: flex;
+    flex-wrap: wrap;
+    width: 420px;
+    height: 420px;
+    background: #35495E;
+    margin: 2% auto 5%;
+    padding-bottom: 0.7027%;
+    border-radius: 1.75%;
+  }
+  .box{
+    background: #41B883;
+    width: 21.6216%;
+    height: 21.6216%;
+    margin-left: 2.7027%;
+    margin-top: 2.7027%;
+    position: relative;
+    border-radius: 7%;
+  }
+</style>
---
+++ src/components/ceil.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        {{number}}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            ceil: {
+                type: Number,
+                default: 2
+            },
+        },
+        data() {
+            return {
+                number: 4,
+                rand: 0
+            }
+        },
+        computed: {
+            newCeil() {
+                this.rand = Math.floor((Math.random() * 2) + 1);
+                console.log(this.rand)
+                return this.rand
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    div{
+        background: rgb(135, 226, 147);
+        font-size: 60px;
+        width: 100%;
+        height: 100%;
+        color: #fff;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+        text-align: justify;
+        font-weight: bold;
+        z-index: 1;
+        border-radius: 7%;
+        -webkit-transform: translateZ(0);
+    }
+</style>
